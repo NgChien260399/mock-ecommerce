53351f711c5787457974be2375c81fdf866bb3e2
Nguyen Sy Hai <kunr97@gmail.com>
2023-08-25 18:08:13 +0700

    25/8

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,96 @@
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import { routes } from "./routes/index";
-import React from "react";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent";
 import FooterComponent from "./components/FooterComponent/FooterComponent";
+import { Container, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function App() {
   return (
     <div>
       <HeaderComponent />
-      <hr />
       <BrowserRouter>
-        <header>
-          <div>
-            <div className="img">
-              <img src="https://canifa.com/assets/images/logo.svg" alt="" />
-            </div>
-            <div className="menu">
-              <ul>
-                <li>
+        <Container maxWidth="xl">
+          <div className="header">
+            <div className="menu-left">
+              <NavLink to="/">
+                <div className="img">
+                  <img src="https://canifa.com/assets/images/logo.svg" alt="" />
+                </div>
+              </NavLink>
+              <div className="menu">
+                <div>
                   <NavLink to="women">Nữ</NavLink>
-                </li>
-                <li>
+                </div>
+                <div className="dropdown">
                   <NavLink to="men">Nam</NavLink>
-                </li>
-                <li>
+                  <div className="dropdown-content">
+                    <NavLink to="men/clothing">Quần áo</NavLink>
+                    <NavLink to="men/shoes">Giày</NavLink>
+                    <NavLink to="men/clothing">Quần áo</NavLink>
+                    <NavLink to="men/shoes">Giày</NavLink>
+                    <NavLink to="men/clothing">Quần áo</NavLink>
+                    <NavLink to="men/shoes">Giày</NavLink>
+                    <NavLink to="men/clothing">Quần áo</NavLink>
+                    <NavLink to="men/shoes">Giày</NavLink>
+                    <NavLink to="men/clothing">Quần áo</NavLink>
+                    <NavLink to="men/shoes">Giày</NavLink>
+                    <NavLink to="men/clothing">Quần áo</NavLink>
+                    <NavLink to="men/shoes">Giày</NavLink>
+                    <NavLink to="men/clothing">Quần áo</NavLink>
+                    <NavLink to="men/shoes">Giày</NavLink>
+                  </div>
+                </div>
+                <div>
                   <NavLink to="child">Trẻ em</NavLink>
-                </li>
-                <li>
-                  <NavLink to="map">Cửa hàng</NavLink>
-                </li>
-                <li>
-                  <NavLink to="login">Tài khoản</NavLink>
-                </li>
-                <li>
-                  <NavLink to="order">Giỏ hàng</NavLink>
-                </li>
-              </ul>
+                </div>
+                <div>
+                  <NavLink to="/">Giá tốt</NavLink>
+                </div>
+                <div>
+                  <NavLink to="/">Canifa Z</NavLink>
+                </div>
+              </div>
+            </div>
+            <div className="menu-right">
+              <div className="search">
+                <TextField
+                  variant="outlined"
+                  label={
+                    <>
+                      {" "}
+                      <SearchIcon /> Tìm kiếm
+                    </>
+                  }
+                />
+              </div>
+              <div className="group-icon">
+                <div className="store">
+                  <div>
+                    <StorefrontIcon />
+                  </div>
+                  <span>Cửa hàng</span>
+                </div>
+                <div className="account">
+                  <div>
+                    <AccountCircleIcon />
+                  </div>
+                  <span>Tài khoản</span>
+                </div>
+                <div className="cart">
+                  <div>
+                    {" "}
+                    <ShoppingCartIcon />
+                  </div>
+                  <span>Giỏ hàng</span>
+                </div>
+              </div>
             </div>
           </div>
-        </header>
-        <hr />
+        </Container>
         <Routes>
           {routes.map((route) => (
             <Route
@@ -50,7 +101,6 @@ function App() {
           ))}
         </Routes>
       </BrowserRouter>
-      <hr />
       <FooterComponent />
     </div>
   );
