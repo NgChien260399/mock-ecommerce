003083f364879a06ba0b3b3277653e4abad38e9d
Nguyen Sy Hai <kunr97@gmail.com>
2023-08-25 18:07:06 +0700

    25/8

---
+++ src/components/ServiceComponent/ServiceComponent.tsx
@@ -0,0 +1,62 @@
+import { Container } from "@mui/material";
+import styles from "./ServiceComponent.module.css";
+
+export default function ServiceComponent() {
+  const dataService = [
+    {
+      title: "Thanh toán khi nhận hàng (COD)",
+      content: "Giao hàng toàn quốc.",
+      url: "https://media.canifa.com/Simiconnector/Service/s/e/service1.png",
+    },
+    {
+      title: "Miễn phí giao hàng",
+      content: "Với đơn hàng trên 599.000đ.",
+      url: "https://media.canifa.com/Simiconnector/Service/s/e/service2.png",
+    },
+    {
+      title: "Đổi hàng miễn phí",
+      content: "Trong 30 ngày kể từ ngày mua.",
+      url: "https://media.canifa.com/Simiconnector/Service/s/e/service3.png",
+    },
+  ];
+
+  return (
+    <Container maxWidth="xl">
+      <div className={styles.service}>
+        {dataService.map((item, index) => (
+          <div key={index} className={styles.item}>
+            <div className={styles.icon}>
+              <img src={item.url} alt="" />
+            </div>
+            <div className="content">
+              <h3>{item.title}</h3>
+              <p>{item.content}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
+}
+
+// return (
+//   <Container maxWidth="xl">
+//     <div className={styles.service}>
+//       <ServiceItem
+//         title="Thanh toán khi nhận hàng (COD)"
+//         content="Giao hàng toàn quốc."
+//         url="https://media.canifa.com/Simiconnector/Service/s/e/service1.png"
+//       />
+//       <ServiceItem
+//         title="Miễn phí giao hàng"
+//         content="Với đơn hàng trên 599.000đ."
+//         url="https://media.canifa.com/Simiconnector/Service/s/e/service2.png"
+//       />
+//       <ServiceItem
+//         title="Đổi hàng miễn phí"
+//         content="Trong 30 ngày kể từ ngày mua."
+//         url="https://media.canifa.com/Simiconnector/Service/s/e/service3.png"
+//       />
+//     </div>
+//   </Container>
+// );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,48 @@
-import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { CardMedia, Divider } from "@mui/material";
+import ServiceItemComponent from "../../components/ServiceComponent/ServiceComponent";
+import HotDeal from "../../components/HotDealComponent/HotDeal";
+import { Fragment } from "react";
 
-const HomePage = () => <div>Home Page</div>;
+const HomePage = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+  return (
+    <Fragment>
+      <Slider {...settings}>
+        <CardMedia
+          component="img"
+          height="100%"
+          image="https://media.canifa.com/Simiconnector/BannerSlider/t/o/top_desktop_2880x960.webp"
+          alt="Your Image Alt Text"
+        />
+        <CardMedia
+          component="img"
+          height="100%"
+          image="https://media.canifa.com/Simiconnector/BannerSlider/h/o/homepage_desktop_2880x960_1.webp"
+          alt="Your Image Alt Text"
+        />
+        <CardMedia
+          component="img"
+          height="100%"
+          image="https://media.canifa.com/Simiconnector/BannerSlider/t/a/tai-app-desktop.webp"
+          alt="Your Image Alt Text"
+        />
+      </Slider>
+      <ServiceItemComponent />
+      <Divider />
+      <HotDeal />
+    </Fragment>
+  );
+};
 
 export default HomePage;
