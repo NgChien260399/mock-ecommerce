337a591c3732ebfcd554dba5058eb0c8df308b3b
unknown <tuanvq72@wru.vn>
2023-09-08 20:52:05 +0700

    tuan

--- src/components/FooterComponent/FooterComponent.tsx
+++ src/components/FooterComponent/FooterComponent.tsx
@@ -1,39 +1,64 @@
+import React, { useState } from 'react';
 import { Button, TextField } from "@mui/material";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import "bootstrap/dist/css/bootstrap.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import styles from "./Footer.module.css";
 
-import styles from "./Footer.module.css"
 const SubscribeForm = () => {
+  const [email, setEmail] = useState('');
+
+  const handleSubscribe = () => {
+    if (!email) {
+      toast.error('Email không được để trống');
+    } else if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(email)) {
+      toast.error('Email đã tồn tại hoặc không đúng định dạng.Vui lòng kiểm tra lại email.');
+    } else {
+      
+
+      toast.success('Cảm ơn bạn đã đăng ký . Hãy kiểm tra e-mail để xác nhận lại với Graceful,bạn nhé ');
+
+      
+      setEmail('');
+    }
+  };
+
   return (
     <div className={styles.form}>
       <div className={styles.formSubscribe}>
         <h2 className={styles.Title}>Đăng ký nhận bản tin</h2>
         <p className={styles.formSubscribeP}>
-          Cùng Canifa Blog cập nhật những thông tin mới nhất về thời trang và
-          phong cách sống.
+          Cùng Graceful Blog cập nhật những thông tin mới nhất về thời trang và phong cách sống.
         </p>
         <div className={styles.inputContainer}>
-          <TextField  type="email" placeholder="Nhập địa chỉ email của bạn" />
+          <ToastContainer />
+          <TextField
+            type="email"
+            placeholder="Nhập địa chỉ email của bạn"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <Button
             variant="contained"
             color="error"
             style={{ borderRadius: "0" }}
             className={styles.footerButton}
+            onClick={handleSubscribe}
           >
             Đăng ký
           </Button>
         </div>
         <div className={styles.socialIcons}>
-          <a href="https://www.facebook.com/canifa.fanpage/"  target="_blank">
+          <a href="https://www.facebook.com/canifa.fanpage/" target="_blank">
             <FacebookIcon />
           </a>
-          <a href="https://www.instagram.com/canifa.fashion/"  target="_blank">
+          <a href="https://www.instagram.com/canifa.fashion/" target="_blank">
             <InstagramIcon />
           </a>
-          <a href="https://www.youtube.com/CANIFAOfficial"  target="_blank">
+          <a href="https://www.youtube.com/CANIFAOfficial" target="_blank">
             <YouTubeIcon />
           </a>
           <a href="#">
@@ -48,30 +73,24 @@ const SubscribeForm = () => {
 const ContactInfo = () => {
   return (
     <div className={`${styles.footerSection} col-sm-4`}>
-      <h3 className={styles.title} >CÔNG TY CỔ PHẦN CANIFA</h3>
+      <h3 className={styles.title}>CÔNG TY CỔ PHẦN Graceful</h3>
       <p>
-        Số ĐKKD: 0107574310, ngày cấp: 23/09/2016, Nơi cấp: Sở Kế hoạch và đầu
+        Số ĐKKD: 9999999, ngày cấp: 1/09/2023, Nơi cấp: Sở Kế hoạch và đầu
         tư Hà Nội
       </p>
-      <p>
-        Địa chỉ trụ sở tại số 688 Đường Quang Trung, Phường La Khê, Quận Hà
-        Đông, Thành phố Hà Nội.
-      </p>
-      <p>
-        Địa chỉ liên hệ: P301, tầng 3, tòa nhà GP Invest, số 170 La Thành,
-        Phường Ô Chợ Dừa, Quận Đống Đa, Thành Phố Hà Nội.
-      </p>
-      <p>Điện thoại: +8424 - 7303.0222</p>
-      <p>Fax: +8424 - 6277.6419</p>
-      <p>Email: hello@canifa.com</p>
+      <p>Địa chỉ trụ sở tại số 17 Duy Tân</p>
+      <p>Địa chỉ liên hệ: 17 Duy Tân</p>
+      <p>Điện thoại: +8424 - ****.****</p>
+      <p>Fax: +8424 - ****.****</p>
+      <p>Email: nhóm4@Graceful.com</p>
       <div className={styles.socialIcons1}>
-        <a className={styles.socialIcons1A} href="https://www.facebook.com/canifa.fanpage/"  target="_blank">
+        <a className={styles.socialIcons1A} href="https://www.facebook.com/canifa.fanpage/" target="_blank">
           <FacebookIcon />
         </a>
-        <a className={styles.socialIcons1A} href="https://www.instagram.com/canifa.fashion/"  target="_blank">
+        <a className={styles.socialIcons1A} href="https://www.instagram.com/canifa.fashion/" target="_blank">
           <InstagramIcon />
         </a>
-        <a className={styles.socialIcons1A} href="https://www.youtube.com/CANIFAOfficial"   target="_blank">
+        <a className={styles.socialIcons1A} href="https://www.youtube.com/CANIFAOfficial" target="_blank">
           <YouTubeIcon />
         </a>
         <a className={styles.socialIcons1A} href="#">
@@ -112,7 +131,7 @@ const BrandLinks = () => {
 
 const SupportLinks = () => {
   return (
-    <div className={`${styles.footerSection} col-sm-3 `}>
+    <div className={`${styles.footerSection} col-sm-3`}>
       <h3 className={styles.title}>HỖ TRỢ</h3>
       <ul>
         <li>
@@ -146,7 +165,7 @@ const SupportLinks = () => {
 
 const AppDownload = () => {
   return (
-    <div className={`${styles.footerSection} col-sm-3 `}>
+    <div className={`${styles.footerSection} col-sm-3`}>
       <div>
         <h3 className={styles.title}>TẢI ỨNG DỤNG</h3>
         <div style={{ display: "flex" }}>
@@ -158,16 +177,16 @@ const AppDownload = () => {
             />
           </div>
           <div className={styles.Apps}>
-            <a className={styles.AppsA} href="https://play.google.com/store/apps/details?id=com.app.canifa&hl=en"  target="_blank">
+            <a className={styles.AppsA} href="https://play.google.com/store/apps/details?id=com.app.canifa&hl=en" target="_blank">
               <img
                 className={`${styles.AppsImg} ${styles.Apps}`}
                 src="https://canifa.com/assets/images/googleplay.png"
                 alt=""
               />
             </a>
-            <a className={styles.AppsA} href="https://apps.apple.com/vn/app/canifa/id1175895653"  target="_blank">
+            <a className={styles.AppsA} href="https://apps.apple.com/vn/app/canifa/id1175895653" target="_blank">
               <img
-                  className={`${styles.AppsImg} ${styles.Apps}`}
+                className={`${styles.AppsImg} ${styles.Apps}`}
                 src="https://canifa.com/assets/images/appstore.png"
                 alt=""
               />
@@ -175,9 +194,9 @@ const AppDownload = () => {
           </div>
         </div>
       </div>
-    
+
       <div>
-        <h3 className={styles.title}>PHƯƠNG THỨC THANH TOÁN</h3>
+        <h3 className={styles.title} style={{ marginTop: "20px" }}>PHƯƠNG THỨC THANH TOÁN</h3>
         <img src="https://canifa.com/assets/images/pay.svg" alt="" />
       </div>
     </div>
@@ -188,15 +207,10 @@ const FooterBottom = () => {
   return (
     <>
       <div className={styles.footerBottom}>
-        <div className="copyright">© 2023 CANIFA</div>
+        <div className="copyright">© 2023 Graceful</div>
         <div className="bct">
-          <a
-            href="http://online.gov.vn/(X(1)S(edqk3incgl5uzlmn5mxjtu0b))/Home/WebDetails/36833?AspxAutoDetectCookieSupport=1"
-            target="_blank"
-            aria-label="Pay"
-          >
+          <a href="http://online.gov.vn/(X(1)S(edqk3incgl5uzlmn5mxjtu0b))/Home/WebDetails/36833?AspxAutoDetectCookieSupport=1" target="_blank" aria-label="Pay">
             <img
-            
               src="https://canifa.com/assets/images/bocongthuong.png"
               alt=""
             />
@@ -214,7 +228,7 @@ const Footer = () => {
         <SubscribeForm />
         <div
           className={styles.footerEnd}
-          style={{ display: "flex", flexWrap: "wrap" }}
+          style={{ display: "flex", flexWrap: "wrap" , background:"b"}}
         >
           <ContactInfo />
           <BrandLinks />
